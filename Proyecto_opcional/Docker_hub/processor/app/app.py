0f21668d9936cf71c9f862334b54e1927b8d1cf4
adrielav2 <adrielaraya@hotmail.com>
2023-02-27 18:09:56 -0600

    Processor -sin elastic-

--- Proyecto_opcional/Docker_hub/processor/app/app.py
+++ Proyecto_opcional/Docker_hub/processor/app/app.py
@@ -17,20 +17,62 @@ from datetime import datetime
 import hashlib
 import json
 
+url = "https://www.ncei.noaa.gov/pub/data/ghcn/daily/all"
+
 hostname = os.getenv('HOSTNAME')
 RABBIT_MQ=os.getenv('RABBITMQ')
 RABBIT_MQ_PASSWORD=os.getenv('RABBITPASS')
 OUTPUT_QUEUE=os.getenv('OUTPUT_QUEUE')
 INPUT_QUEUE=os.getenv('INPUT_QUEUE')
 
+
 def callback(ch, method, properties, body):
     json_object = json.loads(body)
-    localtime = time.localtime()
-    result = time.strftime("%I:%M:%S %p", localtime)
-    msg = "{\"data\": [ {\"msg\":\""+result+"\", \"hostname\": \""+hostname+"\"}]}"
-    json_object["data"].append({"msg": result, "hostname": hostname})
+    filename= json_object.getString('message')
+    urlFile= url + filename
+    checkmd5(urlFile,filename)
     #channel_output.basic_publish(exchange='', routing_key=OUTPUT_QUEUE, body=json.dumps(json_object))
     print(json_object)
+
+def checkmd5(urlFile,filename):
+    hitFile = 1
+    requestFile = requests.get(urlFile)
+    requestFileData = str(requestFile.content)
+    md5File = hashlib.md5(requestFileData.encode())
+
+    # Database connection 
+    MARIAHOST = os.getenv('MARIAHOST')
+    MARIAPORT = os.getenv('MARIAPORT')
+    MARIAUSER = os.getenv('MARIAUSER')
+    MARIAPASS = os.getenv('MARIAPASS')
+    MARIADB = os.getenv('MARIADB')
+
+    # Conexion al servicio de la base de datos Mariadb
+    mariaDatabase = mariadb.connect(
+        host=MARIAHOST,
+        port=int(MARIAPORT),
+        user=MARIAUSER, 
+        password=MARIAPASS,
+        database=MARIADB
+    )
+
+    connection = mariaDatabase.cursor()
+
+    connection.execute("SELECT file_md5 from files WHERE file_md5 = ?", (str(md5File.hexdigest()),))
+    
+    for i in connection:
+        hitFile=0
+         
+    if hitFile:
+        connection.execute('UPDATE files SET file_state=PROCESADO WHERE file_md5 = ?', (str(md5File.hexdigest()),))
+    else:
+        print("Found change in file")
+        ##Almacenar en ElasticSearch en indice files.
+        connection.execute('UPDATE files SET file_state=DESCARGADO WHERE file_name= ?', (filename),)
+        connection.execute('UPDATE files SET file_md5=?', (str(md5File.hexdigest())),'WHERE file_name= ?', (filename),)
+        hitFile=1
+
+    return
 
 
 credentials_input = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
@@ -38,7 +80,8 @@ parameters_input = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credent
 connection_input = pika.BlockingConnection(parameters_input)
 channel_input = connection_input.channel()
 channel_input.queue_declare(queue=INPUT_QUEUE)
-channel_input.basic_consume(queue=INPUT_QUEUE, on_message_callback=callback, auto_ack=True)
+channel_input.basic_consume(queue=INPUT_QUEUE, on_message_callback=callback, auto_ack=True) 
+
 
 '''
 credentials_output = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
