1e2107bb8edda94026e1ded6bbeb942c413810bb
André Araya Vargas <andrearaya1234@gmail.com>
2023-03-10 13:39:34 -0600

    stations_transformation fix fin

--- Proyecto_opcional/Docker_hub/stations_transformation/app/app.py
+++ Proyecto_opcional/Docker_hub/stations_transformation/app/app.py
@@ -24,7 +24,7 @@ MARIADB = os.getenv('MARIADB')
 client = Elasticsearch("https://" + ESENDPOINT + ":9200", basic_auth=("elastic", ESPASSWORD), verify_certs=False)
 
 
-# Función para recibir el msj
+# Función para recibir el msj e interpretarlo
 def callback(ch, method, properties, body):
     message = json.loads(body)
     print("Message received: ")
@@ -50,16 +50,12 @@ def callback(ch, method, properties, body):
         )
         connection = mariaDatabase.cursor()
 
-        print("Obtaining station where station_id = " + station_id)
-        connection.execute(
-            "SELECT * FROM stations WHERE station_id = ?",
-            (station_id,))
-
         print("Adding values...")
         # trae valores de la base de datos
         for j in range(0, len(dicDataList)):
+            print("Obtaining station where station_id = " + station_id)
+            connection.execute("SELECT * FROM stations WHERE station_id = ?",(station_id,))
             for i in connection:
-                print('EXISTE RECORD')
                 # agrega valores del station_id
                 dicDataList[j]['latitude'] = str(i[1])
                 dicDataList[j]['longitude'] = str(i[2])
@@ -78,11 +74,11 @@ def callback(ch, method, properties, body):
         # Actualiza datos en MariaDB
         print("Updating file_state in MariaDB...")
         connection.execute("UPDATE files SET file_state='CON_ESTACION' WHERE file_name=?", (nameFile,))
-        result = nameFile
+        result = fileName
 
         print("Enviando mensaje con RabbitMQ...")
         msg = "{\"data\": [ {\"msg\":\"" + result + "\", \"hostname\": \"" + hostname + "\"}]}"
-        channel_output.basic_publish(exchange='', routing_key=OUTPUT_QUEUE, body=msg)
+        channel.basic_publish(exchange='', routing_key=OUTPUT_QUEUE, body=msg)
 
         mariaDatabase.commit()
         mariaDatabase.close()
@@ -92,19 +88,14 @@ def callback(ch, method, properties, body):
 
 
 # RabbitMQ connection
-credentials_input = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
-parameters_input = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials_input)
-connection_input = pika.BlockingConnection(parameters_input)
-channel_input = connection_input.channel()
-channel_input.queue_declare(queue=INPUT_QUEUE)
-channel_input.basic_consume(queue=INPUT_QUEUE, on_message_callback=callback, auto_ack=True)
-
-credentials_output = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
-parameters_output = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials_output)
-connection_output = pika.BlockingConnection(parameters_output)
-channel_output = connection_output.channel()
-channel_output.queue_declare(queue=OUTPUT_QUEUE)
+credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
+parameters = pika.ConnectionParameters(host = RABBIT_MQ, credentials = credentials, heartbeat = 600)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+channel.queue_declare(queue = INPUT_QUEUE)
+channel.queue_declare(queue = OUTPUT_QUEUE)
+channel.basic_consume(queue = INPUT_QUEUE, on_message_callback = callback, auto_ack = True)
 
 print('Esperando cola...')
 
-channel_input.start_consuming()
+channel.start_consuming()
